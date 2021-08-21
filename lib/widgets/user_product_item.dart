091d80845623e2322e83810bb2202b6dc534105d
ankit <69717009+crew-guy@users.noreply.github.com>
2021-08-21 19:55:21 +0530

    delete error snackbar

--- lib/widgets/user_product_item.dart
+++ lib/widgets/user_product_item.dart
@@ -16,6 +16,7 @@ class UserProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final scaffold = ScaffoldMessenger.of(context);
     return ListTile(
       leading: CircleAvatar(
         backgroundImage: NetworkImage(imgUrl),
@@ -35,8 +36,18 @@ class UserProductItem extends StatelessWidget {
             ),
             IconButton(
               icon: Icon(Icons.delete),
-              onPressed: () {
-                Provider.of<Products>(context, listen: false).deleteProduct(id);
+              onPressed: () async {
+                try {
+                  await Provider.of<Products>(context, listen: false)
+                      .deleteProduct(id);
+                } catch (e) {
+                  scaffold.showSnackBar(
+                    SnackBar(
+                      content:
+                          Text('Deleting failed', textAlign: TextAlign.center),
+                    ),
+                  );
+                }
               },
               color: Theme.of(context).errorColor,
             ),
