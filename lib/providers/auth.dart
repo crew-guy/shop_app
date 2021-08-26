9978adef18fd90b3dfe31c6c1e110223f8db5511
ankit <69717009+crew-guy@users.noreply.github.com>
2021-08-26 10:00:09 +0530

    refac

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -10,9 +10,13 @@ class Auth with ChangeNotifier {
   DateTime? _expiryToken;
   String? _userId;
 
-  Future<void> signup(String email, String password) async {
+  Future<void> _authenticate(
+    String email,
+    String password,
+    String urlKey,
+  ) async {
     var url = Uri.parse(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${WEB_API_KEY}');
+        'https://identitytoolkit.googleapis.com/v1/accounts:${urlKey}?key=${WEB_API_KEY}');
     final response = await http.post(
       url,
       body: json.encode(
@@ -22,19 +26,11 @@ class Auth with ChangeNotifier {
     print(json.decode(response.body));
   }
 
+  Future<void> signup(String email, String password) async {
+    return _authenticate(email, password, 'signUp');
+  }
+
   Future<void> signin(String email, String password) async {
-    final url = Uri.parse(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${WEB_API_KEY}');
-    final response = await http.post(
-      url,
-      body: json.encode(
-        {
-          'email': email,
-          'password': password,
-          'returnSecureToken': true,
-        },
-      ),
-    );
-    print(json.decode(response.body));
+    return _authenticate(email, password, 'signInWithPassword');
   }
 }
