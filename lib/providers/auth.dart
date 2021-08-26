edfb9b23f70214cae2f504de7630f02f24b8a9ed
ankit <69717009+crew-guy@users.noreply.github.com>
2021-08-26 09:53:28 +0530

    login flow fin

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import "package:http/http.dart" as http;
 import 'dart:convert';
 
+const WEB_API_KEY = 'AIzaSyAhvn-Ig5Y9NoNnvBrqTm4RcpM590bxzkI';
+
 class Auth with ChangeNotifier {
   String? _token;
   DateTime? _expiryToken;
@@ -10,11 +12,27 @@ class Auth with ChangeNotifier {
 
   Future<void> signup(String email, String password) async {
     var url = Uri.parse(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAhvn-Ig5Y9NoNnvBrqTm4RcpM590bxzkI');
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${WEB_API_KEY}');
     final response = await http.post(
       url,
       body: json.encode(
         {'email': email, 'password': password, 'returnSecureToken': true},
+      ),
+    );
+    print(json.decode(response.body));
+  }
+
+  Future<void> sigin(String email, String password) async {
+    final url = Uri.parse(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${WEB_API_KEY}');
+    final response = await http.post(
+      url,
+      body: json.encode(
+        {
+          'email': email,
+          'password': password,
+          'returnSecureToken': true,
+        },
       ),
     );
     print(json.decode(response.body));
