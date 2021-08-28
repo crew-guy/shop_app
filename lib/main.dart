8f46c240ce3bba3e7322a6514d482684b5cce421
ankit <69717009+crew-guy@users.noreply.github.com>
2021-08-28 17:42:59 +0530

    minor fix

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,12 @@ import 'package:shop_app/providers/products.dart';
 import 'package:shop_app/screens/auth_screen.dart';
 import 'package:shop_app/screens/cart_screen.dart';
 import 'package:shop_app/screens/orders_screen.dart';
+import 'package:shop_app/screens/splash_screen.dart';
 import 'package:shop_app/screens/user_products_screen.dart';
 import 'package:shop_app/screens/edit_products_screen.dart';
 import 'package:shop_app/screens/product_details_screen.dart';
 import 'package:shop_app/screens/products_overview_screen.dart';
+import "package:shared_preferences/shared_preferences.dart";
 
 void main() {
   runApp(MyApp());
@@ -20,6 +22,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SharedPreferences.setMockInitialValues({});
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (ctx) => Auth()),
@@ -59,7 +62,15 @@ class MyApp extends StatelessWidget {
               accentColor: Colors.grey,
               fontFamily: 'Lato',
             ),
-            home: auth.isAuth ? ProductsOverviewScreen() : AuthScreen(),
+            home: auth.isAuth
+                ? ProductsOverviewScreen()
+                : FutureBuilder(
+                    future: auth.tryAutoLogin(),
+                    builder: (ctx, authSnapshot) =>
+                        authSnapshot.connectionState == ConnectionState.waiting
+                            ? SplashScreen()
+                            : AuthScreen(),
+                  ),
             // initialRoute: AuthScreen.routeName,
             routes: {
               AuthScreen.routeName: (ctx) => AuthScreen(),
